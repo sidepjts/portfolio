e71029a710ffee4af030f760e97f36e622172c0a
carlobutelli <dev.butelli@gmail.com>
2019-12-01 21:03:35 +0100

    Added landing page

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import {Cell, Grid} from "react-mdl";
 
 class Home extends Component {
     render() {
         return(
-            <div><h1>Home Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className='home-grid'>
+                    <Cell col={12}>
+                        <img
+                            src={process.env.PUBLIC_URL + '/images/profile.jpg'}
+                            alt='avatar'
+                            className='avatar-img'
+                        />
+                        <div className='banner-text'>
+                            <h1>Software Engineer</h1>
+                            <hr/>
+                            <p>Java | Python | Angular4 | React | Databases | DevOps | AWS</p>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
