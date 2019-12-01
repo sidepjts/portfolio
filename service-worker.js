be3e54a4bb6e7f36a149a2683ae268a39de5393b
carlobutelli <dev.butelli@gmail.com>
2019-12-01 21:26:44 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f7e54cc362a14c422f19abdbec561412.js"
+  "/portfolio/precache-manifest.692c422649d520abc94f596aee220b41.js"
 );
 
 self.addEventListener('message', (event) => {
